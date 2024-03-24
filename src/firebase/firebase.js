18f61efe168f2f96f5df3efd9ad506c68d83d4f3
parthkumar9 <parthkumar9@outlook.com>
2024-03-24 12:39:46 -0600

    auth-functional

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,14 +9,14 @@ import {getAuth} from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCHOqZf9Be1tr8hz5QfpiyxBjbzy_yez6w",
-  authDomain: "mernauth-demo.firebaseapp.com",
-  projectId: "mernauth-demo",
-  storageBucket: "mernauth-demo.appspot.com",
-  messagingSenderId: "273755815159",
-  appId: "1:273755815159:web:d14fdb471e0f42105ce7f2",
-  measurementId: "G-D4JN8ZMP29"
-};
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  // measurementId: NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
